290d81f9523de91bc17bb558237c46debfb30bda
J0hn <j0hntvdesign@gmail.com>
2019-04-08 17:00:37 +0300

    Update send.py

--- send.py
+++ send.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 from time import sleep
 from instabot import Bot
@@ -7,20 +8,20 @@ from dotenv import load_dotenv
 def send_foto(path, username, password):
     bot = Bot()
     bot.login(username=username, password=password)
-
     images = os.listdir(path)
-
     for image in images:
         bot.upload_photo(f'{path}/{image}')
-        if not bot.api.last_response.ok:
-            print(bot.api.last_response)
 
 
 def main():
     load_dotenv()
     username = os.getenv('USERNAME')
     password = os.getenv('PASSWORD')
-    send_foto('images', username, password)
+    parser = argparse.ArgumentParser(description='Posting to Instagram')
+    parser.add_argument('path', help='Path to images')
+    args = parser.parse_args()
+    path = args.path
+    send_foto(path, username, password)
 
 if __name__ == '__main__':
     main()
