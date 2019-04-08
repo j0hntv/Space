08e6bee65ee69f8852055d06d80450a4da9dc085
J0hn <j0hntvdesign@gmail.com>
2019-04-08 17:01:07 +0300

    Update fetch_spacex.py

--- fetch_spacex.py
+++ fetch_spacex.py
@@ -8,25 +8,20 @@ def fetch_spacex_last_launch(path):
     url = 'https://api.spacexdata.com/v3/launches/latest'
     response = requests.get(url).json()
     images_url = response['links']['flickr_images']
-
-    print(f'Downloads {len(images_url)} images:')
-
     for num, image_url in enumerate(images_url, start=1):
         name = f'SpaceX_{num}.jpg'
-        print(f'{num}. {name}...', end=' ')
         image = requests.get(image_url).content
         with open(f'{path}/{name}', 'wb') as file:
             file.write(image)
-            print('Ok.')
     
-    print('Done.')
-
 
 def main():
     parser = argparse.ArgumentParser(description='Downloading photos of the latest launch SpaceX')
     parser.add_argument('path', help='Path to save images')
     args = parser.parse_args()
+    print('Images download...')
     fetch_spacex_last_launch(args.path)
+    print('Done.')
 
 
 if __name__ == '__main__':
